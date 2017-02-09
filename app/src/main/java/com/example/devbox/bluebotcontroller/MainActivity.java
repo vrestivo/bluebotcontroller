3d3aad633df959d7721d49628349cbc204e6e81b
Vladimir Restivo <vladimir.restivo@gmail.com>
2017-02-09 14:32:21 -0800

    add main fragmet with a button and a text view

--- app/src/main/java/com/example/devbox/bluebotcontroller/MainActivity.java
+++ app/src/main/java/com/example/devbox/bluebotcontroller/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.devbox.bluebotcontroller;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    MainFragment mMainFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mMainFragment = new MainFragment();
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.gui_container, mMainFragment).commit();
+
     }
 }
---
+++ app/src/main/java/com/example/devbox/bluebotcontroller/MainFragment.java
@@ -0,0 +1,25 @@
+package com.example.devbox.bluebotcontroller;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by devbox on 2/9/17.
+ */
+
+public class MainFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //return super.onCreateView(inflater, container, savedInstanceState);
+
+        View rootView  = inflater.inflate(R.layout.fragment_main, container, false);
+
+        return rootView;
+    }
+}
