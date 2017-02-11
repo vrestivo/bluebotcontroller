49f39865af9b45fc496dfd1b65d83e3f6b464444
Vladimir Restivo <vladimir.restivo@gmail.com>
2017-02-10 22:40:44 -0800

    DiscoveryActivity: add onClickListeners for buttons add BroadcastReceiver for the Bluetooth events add button methods ===== Add DeviceListAdapter class Add device_list_item.layout Add strings

---
+++ app/src/main/java/com/example/devbox/bluebotcontroller/DeviceListAdapter.java
@@ -0,0 +1,73 @@
+package com.example.devbox.bluebotcontroller;
+
+import android.database.DataSetObserver;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.ListAdapter;
+
+/**
+ * Created by devbox on 2/10/17.
+ */
+
+public class DeviceListAdapter extends ListAdapter {
+    @Override
+    public boolean areAllItemsEnabled() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled(int position) {
+        return false;
+    }
+
+    @Override
+    public void registerDataSetObserver(DataSetObserver observer) {
+
+    }
+
+    @Override
+    public void unregisterDataSetObserver(DataSetObserver observer) {
+
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return 0;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+}
--- app/src/main/java/com/example/devbox/bluebotcontroller/DiscoveryActivity.java
+++ app/src/main/java/com/example/devbox/bluebotcontroller/DiscoveryActivity.java
@@ -1,11 +1,20 @@
 package com.example.devbox.bluebotcontroller;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -17,6 +26,23 @@ public class DiscoveryActivity extends AppCompatActivity {
     private ListView mPairedDevices;
     private ListView mAvailableDevices;
     private Button mScanButton;
+    private BluetoothAdapter mBtAdapter;
+    private ArrayList<String> mPaired;
+    private ArrayList<String> mAvailable;
+
+
+    private final BroadcastReceiver mBtReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if(action.equals(BluetoothDevice.ACTION_FOUND)){
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                String devName = device.getName();
+                String devAddress = device.getAddress();
+                mAvailable.add(devName + " " + devAddress);
+            }
+        }
+    };
 
 
 
@@ -25,11 +51,72 @@ public class DiscoveryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_discovery);
 
+        mPaired = new ArrayList<>();
+        mAvailable = new ArrayList<>();
+
+        mBtAdapter = BluetoothAdapter.getDefaultAdapter();
         mPairedDevices = (ListView) findViewById(R.id.devices_paired_lv);
         mAvailableDevices = (ListView) findViewById(R.id.devices_available_lv);
         mScanButton = (Button) findViewById(R.id.button_scan);
 
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(mBtReceiver, filter);
+
+        mPairedDevices.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        mAvailableDevices.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(mBtReceiver);
+    }
+
+
+
+    public void clickPaired(){
+        //TODO connect to paired device
+    }
+
+    public void clickAvailable(){
+        //TODO connect to available device
+    }
+
+    public void scan(){
+        if(mBtAdapter.isEnabled()){
+            //check if adapter is already discovering
+            //if so cancel the ongoing discovery
+            if(mBtAdapter.isDiscovering()){
+                mBtAdapter.cancelDiscovery();
+            }
+            mBtAdapter.startDiscovery();
+
+        }
+        else{
+            Toast.makeText(getApplicationContext(),
+                    getString(R.string.bt_error_off),
+                            Toast.LENGTH_SHORT)
+                    .show();
+        }
+    }
+
 
 
 }
