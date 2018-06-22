5a8ad89adffe5f115e8ae2114e4e587b118d683b
Vladimir <vladimir.restivo@gmail.com>
2018-06-22 13:40:18 -0700

    chore:		update UML diagram

--- app/src/main/java/com/example/devbox/bluebotcontroller/view/DiscoveryViewActivity.java
+++ app/src/main/java/com/example/devbox/bluebotcontroller/view/DiscoveryViewActivity.java
@@ -3,8 +3,6 @@ package com.example.devbox.bluebotcontroller.view;
 import android.bluetooth.BluetoothAdapter;
 import android.support.v7.app.AppCompatActivity;
 
-import com.example.devbox.bluebotcontroller.view.joystick.IDiscoveryView;
-
 public class DiscoveryView extends AppCompatActivity implements IDiscoveryView {
 
     @Override
